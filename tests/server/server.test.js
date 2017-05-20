db21bb4295a57a6a52e2e27c99ffc60137bf8fe4
Andrew <andy@andycondon.com>
2017-05-20 16:45:47 +1200

    moving server side code to server folder

--- tests/server/server.test.js
+++ tests/server/server.test.js
@@ -2,7 +2,7 @@ var test = require('ava')
 var request = require('supertest')
 var cheerio = require('cheerio')
 
-var createServer = require('../server')
+var createServer = require('../../server/server')
 
 var configureDatabase = require('./helpers/database-config')
 configureDatabase(test, createServer)
